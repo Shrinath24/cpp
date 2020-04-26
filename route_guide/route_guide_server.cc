c4b3ba8dd6844277c30bfaf1bc549364ae488d5c
Shrinath24 <akshay.shrinath24@gmail.com>
2020-04-25 22:49:36 -0500

    server

--- route_guide/route_guide_server.cc
+++ route_guide/route_guide_server.cc
@@ -47,6 +47,7 @@ using std::chrono::system_clock;
 using routeguide::Input;
 using routeguide::Stats;
 using routeguide::RouteGuide;
+using routeguide::List;
 //using routeguide::GetStatus;
 
 struct square {
@@ -57,34 +58,101 @@ struct square {
 } square;
 list<struct square> workingset;
 std::queue<string> Qservers;
-std::queue<string> Rservers;
+std::queue<string> Lservers;
+std::queue<string> l1servers;
 string Qserver;
 static unsigned long long lastTotalUser, lastTotalUserLow, lastTotalSys, lastTotalIdle;
 string loadBalancerIP;
 int checkForThreshold(){
+	int flag=0;
 	struct square temp;
 	for(auto it = workingset.begin(); it != workingset.end(); ++it){
+			double cpu_metrics = atof(((*it).cpu_metric).c_str());
+			if(cpu_metrics>30.0){
+				flag=1;
+				break;
+			}else{
+				flag=0;
+			}
+		}
+	for(auto it = workingset.begin(); it != workingset.end(); ++it){
 		double cpu_metrics = atof(((*it).cpu_metric).c_str());
-		if(cpu_metrics>0.0){
+		if(cpu_metrics>70.0){
 			return 0;
+		}else if(Lservers.size()>2 && flag==0){
+			return 2;
 		}
 	}
 	//	cout<<get_current_dir()<<"\n";
 	return 1;
 }
 void addServersQueue(){
-	Qservers.push("i-09a734a7f49469fed");
+	//Qservers.push("i-09a734a7f49469fed");
+	Qservers.push("2");
+	Lservers.push("1");
+	Qservers.push("3");
+	Qservers.push("4");
+	Qservers.push("5");
+}
+void writeFile(string content){
+ofstream myfile ("ip-list.txt");
+  if (myfile.is_open()){
+    myfile << content;
+    myfile.close();
+  }
+  else cout << "Unable to open file";
 }
 string getFromQ(){
 	string serverIDQueue = Qservers.front();
+	Lservers.push(serverIDQueue);
+	Qservers.pop();
+	string content;
+	while (!Lservers.empty()){
+				content+=Lservers.front()+",";
+
+				l1servers.push(Lservers.front());
+				Lservers.pop();
+			}
+			while (!l1servers.empty()){
+			//	cout<<" "<<l1servers.front();
+				Lservers.push(l1servers.front());
+				l1servers.pop();
+	}
+	writeFile(content);
 	return serverIDQueue;
 }
 
+string getFromQDown(){
+	string serverIDQueue = Lservers.front();
+	Qservers.push(serverIDQueue);
+	Lservers.pop();
+	string content;
+	while (!Lservers.empty()){
+				content+=Lservers.front()+",";
+				l1servers.push(Lservers.front());
+				Lservers.pop();
+			}
+			while (!l1servers.empty()){
+			//	cout<<" "<<l1servers.front();
+				Lservers.push(l1servers.front());
+				l1servers.pop();
+	}
+	return serverIDQueue;
+}
+int delServer(){
+	string command = "./check_down1.sh";
+	string serverIdFromQ = getFromQDown();
+	command = command +" "+ serverIdFromQ;
+	system(command.c_str());
+}
+
+
+
 int newServer(){
-string command = "./check.sh";
-string serverIdFromQ = getFromQ();
-command = command +" "+ serverIdFromQ+" "+loadBalancerIP;;
-system(command.c_str());
+	string command = "./check1.sh";
+	string serverIdFromQ = getFromQ();
+	command = command +" "+ serverIdFromQ+" "+loadBalancerIP;;
+	system(command.c_str());
 }
 
 int addServer(string serverId, string serverIp, string servercpumet){
@@ -96,6 +164,8 @@ int addServer(string serverId, string serverIp, string servercpumet){
 			(*it).status=checkForThreshold();
 			if((*it).status  == 0){
 				newServer();
+			}else if((*it).status  == 2){
+				delServer();
 			}
 			return 2;
 		}
@@ -103,7 +173,7 @@ int addServer(string serverId, string serverIp, string servercpumet){
 	temp.serverID=serverId;
 	temp.ip=serverIp;
 	temp.cpu_metric=servercpumet;
-    temp.status=checkForThreshold();
+	temp.status=checkForThreshold();
 	workingset.push_front(temp);
 	//system("./check.sh ");
 	return 3;
@@ -111,51 +181,78 @@ int addServer(string serverId, string serverIp, string servercpumet){
 }
 
 class RouteGuideImpl final : public RouteGuide::Service {
- public:
-  explicit RouteGuideImpl(const std::string& db) {
-    //routeguide::ParseDb(db, &feature_list_);
-  }
+public:
+	explicit RouteGuideImpl(const std::string& db) {
+		//routeguide::ParseDb(db, &feature_list_);
+	}
 
-  Status GetStatus(ServerContext* context, const Input* input,
-                    Stats* cpustats) override {
-	  addServer(input->server(), input->ip(), input->cpumet());
-	  newServer();
-      return Status::OK;
-  }
+	Status GetStatus(ServerContext* context, const Input* input,
+			Stats* cpustats) override {
+		addServer(input->server(), input->ip(), input->cpumet());
+		return Status::OK;
+	}
 
-  Status ListServers(ServerContext* context, const Input* input,
+	Status ListServers(ServerContext* context, const Input* input,
 			ServerWriter<Input>* writer) override {
-	  Input inputLoop;
+		Input inputLoop;
 		for(auto it = workingset.begin(); it != workingset.end(); ++it){
-			inputLoop.set_server((*it).serverID);
-			inputLoop.set_ip((*it).ip);
-			inputLoop.set_cpumet((*it).cpu_metric);
-			inputLoop.set_status((*it).status);
-			const Input& inputLoopin = inputLoop;
-			writer->Write(inputLoopin);
+			while (!Lservers.empty()){
+				if(Lservers.front().compare((*it).serverID)==0){
+				inputLoop.set_server((*it).serverID);
+				inputLoop.set_ip((*it).ip);
+				inputLoop.set_cpumet((*it).cpu_metric);
+				inputLoop.set_status((*it).status);
+				const Input& inputLoopin = inputLoop;
+				writer->Write(inputLoopin);
+				}
+				l1servers.push(Lservers.front());
+				Lservers.pop();
+			}
+			while (!l1servers.empty()){
+			//	cout<<" "<<l1servers.front();
+				Lservers.push(l1servers.front());
+				l1servers.pop();
+			}
 		}
+		return Status::OK;
+	}
+	Status ListServerslist(ServerContext* context, const List* list,
+			ServerWriter<List>* writer) override {
+		List listLoop;
+		while (!Lservers.empty()){
+			listLoop.set_serverlist(Lservers.front());
+			const List& listLoopin = listLoop;
+			writer->Write(listLoopin);
+			l1servers.push(Lservers.front());
+			Lservers.pop();
+		}
+		while (!l1servers.empty()){
+		//	cout<<" "<<l1servers.front();
+			Lservers.push(l1servers.front());
+			l1servers.pop();
+		}
+
 		return Status::OK;
 	}
 };
 
 void RunServer(const std::string& db_path) {
-  std::string server_address("0.0.0.0:50051");
-  RouteGuideImpl service(db_path);
-
-  ServerBuilder builder;
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  builder.RegisterService(&service);
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
-  server->Wait();
+	std::string server_address("0.0.0.0:50051");
+	RouteGuideImpl service(db_path);
+	ServerBuilder builder;
+	builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+	builder.RegisterService(&service);
+	std::unique_ptr<Server> server(builder.BuildAndStart());
+	std::cout << "Server listening on " << server_address << std::endl;
+	server->Wait();
 }
 
 int main(int argc, char** argv) {
-  // Expect only arg: --db_path=path/to/route_guide_db.json.
-  std::string db = "";
-  loadBalancerIP=argv[1];
-  addServersQueue();
-  RunServer(db);
+	// Expect only arg: --db_path=path/to/route_guide_db.json.
+	std::string db = "";
+	loadBalancerIP=argv[1];
+	addServersQueue();
+	RunServer(db);
 
-  return 0;
+	return 0;
 }
